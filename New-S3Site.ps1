bda2d68db6b4c508108865e383d5c3c5ed765441
JPZ <jpzdiabolis@gmail.com>
2018-01-15 17:45:22 -0500

    refactored new-s3site.ps1 to throw an exception object

--- New-S3Site.ps1
+++ New-S3Site.ps1
@@ -11,39 +11,30 @@ Param(
     $Region="us-east-1"
 )
 
-# make sure the bucket doesn't already exist, checking for resulting string "NoSuchBucket"
-# redirects stderr to stdout, which in turn is stored in a variable, for checking below:
-$results = aws s3 ls s3://$($Name) 2>&1
-if ($results | Select-String -Pattern "NoSuchBucket") {
-    # if the bucket doesn't already exist, create it and prepare accordingly...
-    Write-Host "Creating new bucket s3://$($Name)"
-    # aws s3 ls s3://fake.org 2>&1 | % { $_ | gm }
-    <#
-        try {
-            $result = aws s3 ls s3://fake.org 2>&1
-            if ($result[0].GetType().Name -eq "ErrorRecord") {
-                throw "Bucket not found"
-            }
-        } catch {
-            
-        }
-    #>
-    $bucket = aws s3 mb s3://$($Name) --region $($Region) 2>&1
-    
-    # if your bucket was created successfully, proceed to configure/upload defaults
-    if ($bucket | Select-String -Pattern "make_bucket") {
-        # configure default minimal viable policy, you MUST use Set-Content to preserve JSON integrity...
-        (Get-Content .\static-site-policy.json).replace('BUCKET_NAME', $Name) | Set-Content .\policy.json
-        if (Test-Path -Path "policy.json") {
-            # upload your site contents
-            write-host (Get-Content "policy.json")
-            aws s3api put-bucket-policy --bucket $Name --policy file://policy.json
-            .\Upload-S3Site.ps1 -Name $Name
-        }
-    }    
-} else {
-    # if the bucket already exists, don't do anything and exit noisily!
-    Write-Host "The specified bucket $Name already exists. Exiting to avoid overwriting accidentally."
-    Exit
-}
+$bucketName = "s3://$($Name)"
 
+# make sure the bucket doesn't already exist, checking for an ErrorRecord
+try {
+    $result = aws s3 ls $bucketName 2>&1
+    if ($result[0].GetType().Name -ne "ErrorRecord") {
+        throw [System.Exception]::new("The specified bucket $bucketName already exists.");
+    }
+} catch {
+    # if the bucket already exists, don't do anything and exit noisily!
+    Write-Error $PSItem.Exception.Message
+    Exit
+} 
+
+$bucket = aws s3 mb $bucketName --region $($Region) 2>&1
+
+# if your bucket was created successfully, proceed to configure/upload defaults
+if ($bucket | Select-String -Pattern "make_bucket") {
+    # configure default minimal viable policy, you MUST use Set-Content to preserve JSON integrity...
+    (Get-Content .\static-site-policy.json).replace('BUCKET_NAME', $Name) | Set-Content .\policy.json
+    if (Test-Path -Path "policy.json") {
+        # upload your site contents
+        write-host (Get-Content "policy.json")
+        aws s3api put-bucket-policy --bucket $Name --policy file://policy.json
+        .\Upload-S3Site.ps1 -Name $Name
+    }
+}
