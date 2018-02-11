5afda82456b499ebe6a8581e81b68e81d1e88e30
JPZ <jpzdiabolis@gmail.com>
2018-02-11 14:39:35 -0500

    cleaned indenting on policy template, added cache-control and comments to upload-s3site.ps1

--- Upload-S3Site.ps1
+++ Upload-S3Site.ps1
@@ -7,10 +7,24 @@ Param(
 )
 
 # [https://docs.aws.amazon.com/cli/latest/reference/s3/cp.html] :: upload boilerplate files
-aws s3 cp . s3://$($Name) --exclude "*" --include "*.html" --recursive --metadata-directive REPLACE --content-type "text/html"
-aws s3 cp . s3://$($Name) --exclude "*" --include "*.css" --recursive --metadata-directive REPLACE --content-type "text/css"
-aws s3 cp . s3://$($Name) --exclude "*" --include "*.js" --recursive --metadata-directive REPLACE --content-type "text/javascript"
-aws s3 cp . s3://$($Name) --exclude "*" --include "*.png" --recursive --metadata-directive REPLACE --content-type "image/png"
-aws s3 cp . s3://$($Name) --exclude "*" --include "*.jpg" --recursive --metadata-directive REPLACE --content-type "image/jpg"
+aws s3 cp . s3://$($Name) --exclude "*" --include "*.html" --recursive --metadata-directive REPLACE `
+    --content-type text/html --cache-control public,max-age=604800
+
+# Upload stylesheets
+aws s3 cp . s3://$($Name) --exclude "*" --include "*.css" --recursive --metadata-directive REPLACE `
+    --content-type text/css --cache-control public,max-age=604800
+
+# Upload JavaScript files
+aws s3 cp . s3://$($Name) --exclude "*" --include "*.js" --recursive --metadata-directive REPLACE `
+    --content-type text/javascript --cache-control public,max-age=604800
+
+# Upload PNG images
+aws s3 cp . s3://$($Name) --exclude "*" --include "*.png" --recursive --metadata-directive REPLACE `
+    --content-type image/png --cache-control public,max-age=604800
+
+# Upload JPG images
+aws s3 cp . s3://$($Name) --exclude "*" --include "*.jpg" --recursive --metadata-directive REPLACE `
+    --content-type image/jpg --cache-control public,max-age=604800
+
 
 aws s3 website s3://$($Name) --index-document index.html --error-document error.html
